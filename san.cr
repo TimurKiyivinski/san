3d31ecd763a62484717af5b394b4489f21396645
Timur Kiyivinski <timur@linux.com>
2017-04-19 23:58:10 +0800

    [DONE] SAN

--- san.cr
+++ san.cr
@@ -1,7 +1,8 @@
 while user_input = gets
+    spacing = ARGV.size > 0 ? ARGV[0] : nil
     user_input.each_char do |char|
         if char != 'e'
-            print " #{char}"
+            print "#{char}#{spacing}"
         end
     end
     print "\n"
